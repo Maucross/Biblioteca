7615a43a6efeb43b09f9a479486f1676c7be7de4
mauricio <mauricioarizaca@usmp.pe>
2018-10-31 00:52:06 -0500

    modelos

--- Models/MvcBibliotecaContext.cs
+++ Models/MvcBibliotecaContext.cs
@@ -1,7 +1,12 @@
-namespace .
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Models
 {
-    public class MvcBibliotecaContext
+    public class MvcBibliotecaContext : DbContext
     {
-        
+        public MvcBibliotecaContext (DbContextOptions<MvcBibliotecaContext> options)
+        : base(options)
+        {}
+        public DbSet<MvcBibliotecaContext.Models.bibliotecario> bibliotecario {get; set;}
     }
 }
