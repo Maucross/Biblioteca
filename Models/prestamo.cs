15cf1314c32e5adeb34ac8eb3f7b3853aa0d7290
cristobal96 <44531415+cristobal96@users.noreply.github.com>
2018-10-31 23:43:23 -0500

    Update prestamo.cs

--- Models/prestamo.cs
+++ Models/prestamo.cs
@@ -7,10 +7,14 @@ namespace Biblioteca.Models
     public class prestamo
     {
         [Key]
+        //Código de Préstamo
         public int cod_prest {get; set;}
+        //Código del Libro
         public int cod_lib {get; set;}
         [Required]
+        //Modalidad
         public string modalidad {get; set;}
+        //Fecha de Reserva
         public DateTime fecha_reserva {get; set;}
     }
 }
