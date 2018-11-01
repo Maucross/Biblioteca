df8cf174e1617a0cf584d0cbac9000c037cfcea3
cristobal96 <44531415+cristobal96@users.noreply.github.com>
2018-10-31 23:36:39 -0500

    Update estudiante.cs

--- Models/estudiante.cs
+++ Models/estudiante.cs
@@ -5,14 +5,20 @@ namespace Biblioteca.Models
 {
     public class estudiante
     {
+        
         [Key]
+        //Codigo
         public int cod_est{get; set;}
         [Required]
+        //Usuario
         public string usuario {get; set;}
-        
+        //Contraseña
         public string contraseña {get; set;}
+        //Nombre
         public string nomb {get; set;}
+        //Apellido
         public string ape {get; set;}
+        //Escuela
         public string escuela {get; set;}
     }
 }
