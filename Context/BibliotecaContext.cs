595e502d7099aa76cfcdf55c6d1e11bfaf98d95e
cristobal96 <44531415+cristobal96@users.noreply.github.com>
2018-11-02 11:28:04 -0500

    Update BibliotecaContext.cs

--- Context/BibliotecaContext.cs
+++ Context/BibliotecaContext.cs
@@ -9,11 +9,15 @@ namespace Biblioteca.Context
         {
 
         }
-
+        //Bibliotecario
         public DbSet<BibliotecaContext> bibliotecario { get; set; }
+        //Estudiante
         public DbSet<estudiante> estudiante {get; set;}
+        //Libros
         public DbSet<libros> libros {get; set;}
+        //Prestamo
         public DbSet<prestamo> prestamo { get; set; }
+        //Reserva
         public DbSet<reserva> reserva { get; set; }
     
 
